707fc52098b5d1cc2e28b28ec7d41f05ff3f7ce4
vagrant <vagrant@vagrant.vm>
2018-08-08 19:21:08 +0000

    Completed database and seed file.

--- seed.py
+++ seed.py
@@ -10,18 +10,53 @@ from model import connect_to_db, db
 from server import app
 from datetime import datetime
 
+# create test data
 
+def load_users():
+    """Load users from users file into database"""
 
+    print("Users")
 
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
 
+    # Read user file and insert data
+    for row in open("seed_data/users.csv"):
+        row = row.rstrip()
+        username, first_name, last_name, email, password, phone_number = row.split(",")
 
+        user = User(username=username, first_name=first_name, last_name=last_name, 
+                    email=email, password=password, phone_number=phone_number)
 
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
 
+    # Once we're done, commit work
+    db.session.commit()
 
+def load_contacts():
+    """Load contacts from contacts file into database"""
 
+    print("Contacts")
 
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Contact.query.delete()
 
+    # Read user file and insert data
+    for row in open("seed_data/contacts.csv"):
+        row = row.rstrip()
+        user_id, contact_phone_number, relationship, contact_name = row.split(",")
 
+        contact = Contact(user_id=user_id, contact_phone_number=contact_phone_number, 
+                        relationship=relationship, contact_name=contact_name)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(contact)
+
+    # Once we're done, commit work
+    db.session.commit()
 
 
 if __name__ == "__main__":
@@ -30,4 +65,8 @@ if __name__ == "__main__":
     # In case tables haven't been created, create them
     db.create_all()
 
-    # Import different types of data
+    # Import different types of data, comment out when don't want to poplulate db
+    load_users()
+    load_contacts()
+
+
